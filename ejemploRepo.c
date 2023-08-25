51ff96ea2f31e7e0b437d01735b909566834f70e
Abner Requejo L. <jairrequejo@hotmail.com>
2023-08-25 03:50:35 -0500

    [ADD] Arreglo de numeros para insercion creado

--- ejemploRepo.c
+++ ejemploRepo.c
@@ -8,10 +8,24 @@ int main()
 
     int numerosBurbuja[100];
 
+    printf("Arreglo inicial para burbuja: \n");
+
     for(int i = 1; i < 100; i++)
     {
         numerosBurbuja[i] = rand() % 100;
         printf("%d", numerosBurbuja[i]);
+    }
+
+    printf("\n");
+
+    int numerosInsercion[100];
+
+    printf("Arreglo inicial para insercion: \n");
+
+    for(int i = 1; i < 100; i++)
+    {
+        numerosInsercion[i] = rand() % 100;
+        printf("%d", numerosInsercion[i]);
     }
     return 0;
 }
